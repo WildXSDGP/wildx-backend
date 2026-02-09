cabba5afe17bdc16e6cc0d55e2c6a758a8e87b6a
Banuka Malshan <bmagamulla@gmail.com>
2026-02-09 14:13:18 +0530

    Initial Spring Boot backend setup

---
+++ src/main/java/com/sdgp/backend/wildx/WildxApplication.java
@@ -0,0 +1,13 @@
+package com.sdgp.backend.wildx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sdgp/backend/wildx/WildxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdgp.backend.wildx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
