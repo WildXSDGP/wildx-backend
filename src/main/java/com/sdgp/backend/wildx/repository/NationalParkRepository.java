d2781bc2e125bf6b768fe1efb3891b2e9831c6db
Banuka Malshan <bmagamulla@gmail.com>
2026-02-14 13:54:53 +0530

    Create abstract methos  to return Liat of NationalPark objects, that contain user input animal.

--- src/main/java/com/sdgp/backend/wildx/repository/NationalParkRepository.java
+++ src/main/java/com/sdgp/backend/wildx/repository/NationalParkRepository.java
@@ -1,8 +1,23 @@
 package com.sdgp.backend.wildx.repository;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.sdgp.backend.wildx.model.*;
 
 public interface NationalParkRepository extends JpaRepository<NationalPark,Long>{
+	
+	
+	// User Enter specific animal and this method queries and displays National Parks  that contain that animal
+	@Query(value="""
+			SELECT DISTINCT P.*
+			FROM national-parks  P JOIN park_animal_type T 
+			ON P.id= T.park_id
+			WHERE T.animal_type = : animalType
+			
+			""",nativeQuery = true)
+	List<NationalPark> findParksByAnimalType(@Param("animalType") String animalType);
 
 }
