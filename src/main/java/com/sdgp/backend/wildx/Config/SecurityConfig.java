0062f07d631d396786eaf983bfacb83d68825e66
Banuka Malshan <bmagamulla@gmail.com>
2026-03-17 12:11:08 +0530

    Implement sequrity configuration

--- src/main/java/com/sdgp/backend/wildx/Config/SecurityConfig.java
+++ src/main/java/com/sdgp/backend/wildx/Config/SecurityConfig.java
@@ -1,8 +1,25 @@
 package com.sdgp.backend.wildx.Config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/parks/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .httpBasic(httpBasic -> {});
+
+        return http.build();
+    }
 
 }
