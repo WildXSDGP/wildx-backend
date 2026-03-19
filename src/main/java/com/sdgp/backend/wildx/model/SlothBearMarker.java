a2c70a2c38ad06102a8bd8f098d1d8f9b849376a
Dewthilini-D <dewthilini6@gmail.com>
2026-03-19 13:38:56 +0530

    Implement SlothBearMarker class.

---
+++ src/main/java/com/sdgp/backend/wildx/model/SlothBearMarker.java
@@ -0,0 +1,37 @@
+package com.sdgp.backend.wildx.model;
+
+import com.sdgp.backend.wildx.model.AnimalMarker;
+import com.sdgp.backend.wildx.model.NationalPark;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import java.time.LocalDateTime;
+
+@Entity
+@DiscriminatorValue("SLOTH_BEAR")
+public class SlothBearMarker extends AnimalMarker{
+	
+	public SlothBearMarker() {
+	        super();
+	}
+	
+	public SlothBearMarker(NationalPark nationalPark, Double latitude, Double longitude,
+            LocalDateTime spottedAt, String reporterName, String notes) {
+        super(nationalPark, latitude, longitude, spottedAt, reporterName, notes);
+    }
+
+    public SlothBearMarker(NationalPark nationalPark, Double latitude, Double longitude,
+            String reporterName, String notes) {
+        super(nationalPark, latitude, longitude, reporterName, notes);
+    }
+
+    public SlothBearMarker(NationalPark nationalPark, Double latitude, Double longitude) {
+        super(nationalPark, latitude, longitude);
+    }
+
+	@Override
+	public String getAnimalType() {
+		
+		return "Sloth Bear";
+	}
+
+}
