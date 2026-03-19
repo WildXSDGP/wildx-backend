25f82d26353274ae9c5fd5dcb2de1fa0dd910382
Banuka Malshan <bmagamulla@gmail.com>
2026-03-19 13:56:02 +0530

    initiate AnimalMarkerRepository to implement query methods

---
+++ src/main/java/com/sdgp/backend/wildx/repository/AnimalMarkerRepository.java
@@ -0,0 +1,8 @@
+package com.sdgp.backend.wildx.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.sdgp.backend.wildx.model.AnimalMarker;
+
+public interface AnimalMarkerRepository extends JpaRepository<AnimalMarker, Long>{
+
+}
