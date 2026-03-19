dd0eae6a41487029135d837e0d30ce6392132946
Dewthilini-D <dewthilini6@gmail.com>
2026-03-19 12:02:06 +0530

    Add  imports

--- src/main/java/com/sdgp/backend/wildx/model/AsianElephantMarker.java
+++ src/main/java/com/sdgp/backend/wildx/model/AsianElephantMarker.java
@@ -1,6 +1,20 @@
 package com.sdgp.backend.wildx.model;
 
+import com.sdgp.backend.wildx.model.AnimalMarker;
+import com.sdgp.backend.wildx.model.NationalPark;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import java.time.LocalDateTime;
+
+@Entity
 public class AsianElephantMarker extends AnimalMarker {
+	
+	
+	 public AsianElephantMarker() {
+	        super();
+	    }
+	
+	
 
 	@Override
 	public String getAnimalType() {
