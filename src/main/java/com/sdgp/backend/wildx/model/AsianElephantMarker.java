c6542742b726f756cfdaf190a495ffcabe447ad7
Dewthilini-D <dewthilini6@gmail.com>
2026-03-19 11:58:12 +0530

    create AsianElephantMarker class inherited AnimalMarker class

---
+++ src/main/java/com/sdgp/backend/wildx/model/AsianElephantMarker.java
@@ -0,0 +1,11 @@
+package com.sdgp.backend.wildx.model;
+
+public class AsianElephantMarker extends AnimalMarker {
+
+	@Override
+	public String getAnimalType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
