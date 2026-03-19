e2b0cf39f4f0d519901dc9ecc1de5757be423467
Banuka Malshan <bmagamulla@gmail.com>
2026-03-19 14:01:01 +0530

    Initiate findByNationalParkId method to return list of markers

--- src/main/java/com/sdgp/backend/wildx/repository/AnimalMarkerRepository.java
+++ src/main/java/com/sdgp/backend/wildx/repository/AnimalMarkerRepository.java
@@ -1,8 +1,15 @@
 package com.sdgp.backend.wildx.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.sdgp.backend.wildx.model.AnimalMarker;
 
+
+@Repository
 public interface AnimalMarkerRepository extends JpaRepository<AnimalMarker, Long>{
+	List<AnimalMarker> findByNationalParkId(Long parkId);
 
 }
