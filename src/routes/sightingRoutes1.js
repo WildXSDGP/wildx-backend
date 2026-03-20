8ee900d9e1530527a472b4287305d8e0a6af9dff
Savith29 <savithransika29@gmail.com>
2026-03-21 00:16:21 +0530

    register sightingRoutes1 with input validation for create endpoint

---
+++ src/routes/sightingRoutes1.js
@@ -0,0 +1,22 @@
+const express    = require('express');
+const { body }   = require('express-validator');
+const controller = require('../controllers/sightingController1');
+
+const router = express.Router();
+
+// Validation rules
+const createRules = [
+  body('animalType')   .notEmpty().withMessage('animalType is required'),
+  body('locationName') .notEmpty().withMessage('locationName is required'),
+  body('latitude')     .isFloat({ min: -90,  max: 90  }).withMessage('Invalid latitude'),
+  body('longitude')    .isFloat({ min: -180, max: 180 }).withMessage('Invalid longitude'),
+];
+
+router.get  ('/',           controller.getAll);
+router.get  ('/stats',      controller.getStats);
+router.get  ('/:id',        controller.getById);
+router.post ('/', createRules, controller.create);
+router.put  ('/:id/status', controller.updateStatus);
+router.delete('/:id',       controller.delete);
+
+module.exports = router;
