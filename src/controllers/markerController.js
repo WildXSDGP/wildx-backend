e05ab2f2886fc59ebdbd0f2950006ae8fd4a4b64
Banuka Malshan <bmagamulla@gmail.com>
2026-03-23 10:59:48 +0530

    create animal types array and Animal display names

--- src/controllers/markerController.js
+++ src/controllers/markerController.js
@@ -1,1 +1,15 @@
 const { query } = require('../config/db');
+
+const VALID_ANIMAL_TYPES = [
+  'SRI_LANKAN_LEOPARD', 'ASIAN_ELEPHANT', 'SPOTTED_DEER',
+  'CROCODILE', 'WATER_BUFFALO', 'SLOTH_BEAR',
+];
+
+const ANIMAL_DISPLAY_NAMES = {
+  SRI_LANKAN_LEOPARD: 'Sri Lankan Leopard',
+  ASIAN_ELEPHANT:     'Asian Elephant',
+  SPOTTED_DEER:       'Spotted Deer',
+  CROCODILE:          'Crocodile',
+  WATER_BUFFALO:      'Water Buffalo',
+  SLOTH_BEAR:         'Sloth Bear',
+};
